392c2d7204bdf702c96106479b0c3397488ae387
griha1796 <69796095+griha1796@users.noreply.github.com>
2021-01-13 23:09:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+new fullpage('#fullpage', {
+ navigation:true,
+        anchors:['firstPage','secondPage','3rdPage', '4rdPage', '5thPage', '6thPage', 'lastPage'],
+        menu: '#myMenu',
+         scrollOverflow: true ,
+        scrollingSpeed: 700,
+        fitToSectionDelay: 700,
+});
+
+ 
